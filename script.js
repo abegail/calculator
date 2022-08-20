aa194e14ffa5b5e98dac5ab4eb7480c621c77b54
Abegail Pates <abegail.pates@gmail.com>
2022-08-20 19:23:32 +0800

    Create function operate that calls the other calculator functions

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+function operate(operator, firstOperand, secondOperand) {
+    console.log(operator);
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(firstOperand, secondOperand);
+            break;
+        case '-':
+            result = subtract(firstOperand, secondOperand);
+            break;
+        case '*':
+            result = multiply(firstOperand, secondOperand);
+            break;
+        case '/':
+            result = divide(firstOperand, secondOperand);
+            break;
+    }
+    return result;
+}
