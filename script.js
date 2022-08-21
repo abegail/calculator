1e2ee812856a638eb026050aeb6ba3b2448820e7
Abegail Pates <abegail.pates@gmail.com>
2022-08-21 21:48:51 +0800

    Wire operator buttons to functions that perform their respective operations

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const display = document.querySelector("#display");
-const zero = document.querySelector("#zero");
 /*
 const one = document.querySelector("#1");
 const two = document.querySelector("#2");
@@ -12,20 +11,70 @@ const eight = document.querySelector("#8");
 const nine = document.querySelector("#9");
 */
 
-const digits = document.querySelectorAll(".digit");
+const digits = document.querySelectorAll("button");
 console.log(digits);
 
 digits.forEach(digit => {
-    digit.addEventListener('click', (digit) => storeValue(digit));
-    console.log(digit.attributes.id.nodeValue);
+    digit.addEventListener('click', () => {
+        helper(digit);
+    });
+//    console.log(digit.attributes.id.nodeValue);
 });
 
-function storeValue(digit) {
-    display.textContent = digit.attributes.id.nodeValue;
+let valueContainer = ''
+let firstOperand;
+let secondOperand;
+let operator;
+
+function helper(digit){
+
+    let value = digit.attributes.id.nodeValue;
+    switch(value) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            valueContainer += value;
+            display.textContent = valueContainer;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            firstOperand = valueContainer;
+            valueContainer = '';
+            operator = value;
+            break;
+        case 'equals':
+            secondOperand = valueContainer;
+            valueContainer = '';
+            display.textContent = operate(operator, parseInt(firstOperand), parseInt(secondOperand));
+            console.log(operate(operator, parseInt(firstOperand), parseInt(secondOperand)));
+            break;
+        case 'clear':
+            display.textContent = '0';
+            valueContainer = '';
+            firstOperand = '';
+            secondOperand = '';
+            operator = '';
+    }
+
+    console.log(digit.attributes.id.nodeValue);
 }
 
-function showValue() {
+function storeValue(digit) {
+    let value = digit.attributes.id.nodeValue;
+    console.log(value);
+}
 
+function showValue(digit) {
+    display.textContent = digit.attributes.id.nodeValue;
 }
 
 
